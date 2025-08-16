60841688360656778b15208e39706559cd70ec74
Vishalvg7897 <153381941+Vishalvg7897@users.noreply.github.com>
2025-08-16 18:45:45 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
-# Don't Remove Credit Tg - @VJ_Botz
+# Don't Remove Credit Tg - Mr Unknown
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
@@ -7,3 +7,4 @@ from os import environ
 API_ID = int(environ.get("API_ID", "26269752"))
 API_HASH = environ.get("API_HASH", "4d078be87bebb1c0fba48dab67cf1a77")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
+
